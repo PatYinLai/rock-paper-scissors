49cd27ca40fdf8073bbcaeef631133e98bc3e695
PatYinLai <93857052+PatYinLai@users.noreply.github.com>
2021-11-25 20:58:53 +1100

    script.js
    Debugged and made the rock-paper-scissors work off functions.

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-let user = prompt("Best out of 5 games. Please enter rock, paper, or scissors");
-let computer = Math.floor(Math.random()*3); 
-  
-if (computer === 0){
-  computer = 'rock';
-} else if (computer === 1) {
-  computer = 'scissors'; 
-  } else {
-    computer = 'paper';
-  }
+let userSelection ='';
+let computerSelection = '';
+let winCount = 0; 
+
+
+function userPrompt () {
+  userSelection = prompt("Best out of 5 games with the computer! Please enter rock, paper, or scissors");
+  return userSelection.toLowerCase();
+} 
+
+function computerAction() {
+    let computerOptions = ['rock','scissors','paper'];
+    computerSelection = computerOptions[Math.floor(Math.random()*3)]
+    return computerSelection;
+} 
 
 
 for (let i = 0; i < 5; i++) {
-  if (user === computer) {
-    console.log(`You and the computer both used ${user}. This game is a draw`);
-  } else if (user === 'rock' && computer === 'scissors') {
-    console.log('You have used rock while the computer used scissors. You win!');} 
-    else if (user === 'scissors' && computer == 'paper') {
-    console.log('You have used scissors while the computer used paper. You win!');
-  } else if (user === 'paper' && computer == 'rock') {
-    console.log('You have used paper while the computer used rock. You win!');
-  } else {
-    console.log(`You have used ${user} and the computer has used ${computer}. You lose!`);
+  computerAction();
+  userPrompt();
+
+  if (userSelection !== 'rock' && userSelection !== 'scissors' && userSelection !== 'paper' ){
+    console.log ('You did not select rock, paper or scissors.');
+    break;
   }
-}
+ 
+  if (userSelection === computerSelection) {
+    console.log(`You and the computer both used ${userSelection}. This game is a draw`);
+    } else if (userSelection === 'rock' && computerSelection === 'scissors') {
+      winCount += 1;
+      console.log(`You have used rock while the computer used scissors. You win! Your wincount is ${winCount}.`);
+    
+    } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+      winCount += 1;
+      console.log(`You have used scissors while the computer used paper. You win! Your wincount is ${winCount}.`);
+
+    } else if (userSelection === 'paper' && computerSelection === 'rock') {
+      winCount += 1;
+      console.log(`You have used paper while the computer used rock. You win!Your wincount is ${winCount}.`);
+
+    } else {
+      console.log(`You have used ${userSelection} and the computer has used ${computerSelection}. You lose!`);
+    }
+ }
+
+ if (winCount >=3) {
+   console.log(`You have won ${winCount} out of 5 times.You have beaten the computer!'`);
+ } else {
+   console.log(`You have won ${winCount} out of 5 times. Sorry, you have not beaten the computer.`);
+ }
